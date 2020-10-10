e258f4081abeeeda6c663844863c3aacafdd7f10
Jadson Luan <jadsonluan1@gmail.com>
2020-10-10 16:25:31 -0300

    Add initial files

---
+++ a-mazer.js
@@ -0,0 +1,181 @@
+const WALL = 0;
+const DOOR = 1;
+const DEFAULT_LABEL = 0;
+const ENTRANCE_LABEL = 1;
+const EXIT_LABEL = 2;
+
+const Direction = {
+  LEFT: 'left',
+  TOP: 'top',
+  RIGHT: 'right',
+  BOTTOM: 'bottom'
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function choice(array) {
+  const index = Math.floor(Math.random() * array.length);
+  return array[index];
+}
+
+function opposite(direction) {
+  switch(direction) {
+    case Direction.LEFT: return Direction.RIGHT;
+    case Direction.TOP: return Direction.BOTTOM;
+    case Direction.RIGHT: return Direction.LEFT;
+    case Direction.BOTTOM: return Direction.TOP;
+    default: return direction;
+  }
+}
+
+function getAdjacentPosition(position, direction) {
+  switch(direction) {
+    case Direction.LEFT: return { row: position.row, col: position.col - 1};
+    case Direction.TOP: return { row: position.row - 1, col: position.col };
+    case Direction.RIGHT: return { row: position.row, col: position.col + 1};
+    case Direction.BOTTOM: return { row: position.row + 1, col: position.col};
+    default: return position;
+  }
+}
+
+function random(end) {
+  return Math.floor(Math.random() * end);
+}
+
+class Cell {
+  constructor(left, top, right, bottom, label) {
+    this.left = left;
+    this.top = top;
+    this.right = right;
+    this.bottom = bottom;
+    this.label = label;
+  }
+}
+
+class Maze {
+  constructor(size) {
+    this.size = size;
+    this.entrance = { row: random(size), col: 0 };
+    this.exit = { row: random(size), col: size - 1 };
+    this.maze = this.createMaze(size);
+    this.init(this.maze);
+  }
+
+  createMaze(size) {
+    const maze = [];
+    for (let i = 0; i < size; i++) {
+      maze[i] = [];
+      for (let j = 0; j < size; j++) {
+        maze[i][j] = new Cell(WALL, WALL, WALL, WALL, DEFAULT_LABEL) 
+      }
+    }
+
+    return maze;
+  }
+
+  init() {
+    console.log("init");
+    this.maze.forEach((row, i) => {
+      row.forEach((element, j) => {
+        this.randomizeWalls({row: i, col: j});
+      })
+    })
+
+    console.log("entrance", this.entrance);
+    console.log("exit", this.exit);
+
+    // this.makeSolvable();
+
+    // this.maze[this.entrance.row][this.entrance.col].label = ENTRANCE_LABEL;
+    // this.maze[this.exit.row][this.exit.col].label = EXIT_LABEL;
+  }
+
+  async makeSolvable() {
+    // await sleep(10000);
+    const solved = await this.floodfill(this.entrance, this.exit, 3);
+    if (solved) {
+      alert("[✅] Esse labirinto tem solução!");
+    } else {
+      alert("[❌] Esse labirinto não possui solução.");
+    }
+  }
+
+  async floodfill(from, to, replacementLabel) {
+    await sleep(100)
+
+    if (from.row == to.row && from.col == to.col) return true;
+    if (!this.inRange(from)) return false;
+    if (this.getCell(from).label == replacementLabel) return false;
+
+    const current = this.getCell(from);
+    current.label = replacementLabel;
+
+    const directions = [Direction.LEFT, Direction.TOP, Direction.RIGHT, Direction.BOTTOM];
+
+    for (let i = 0; i < directions.length; i++) {
+      let direction = directions[i];
+      let adjacent = getAdjacentPosition(from, direction);
+      let connectedByDoor = current[direction] == DOOR;
+
+      if (this.inRange(adjacent) && connectedByDoor) {
+        let solved = await this.floodfill(adjacent, to, replacementLabel);
+        if (solved) 
+          return true;
+      }
+    }
+
+    return false;
+  }
+
+  randomizeWalls(position) {
+    const directions = [Direction.LEFT, Direction.TOP, Direction.RIGHT, Direction.BOTTOM];
+    
+    directions.forEach(direction => {
+      const shouldCreateDoor = choice([true, false, false]);
+      if (shouldCreateDoor) {
+        this.createDoor(position, direction);
+      }
+    })
+  }
+
+  inRange(position) {
+    const row = position.row;
+    const col = position.col;
+    return (row >= 0 && row < this.size) && (col >= 0 && col < this.size);
+  }
+
+  createDoor(position, direction) {
+    if (this.hasBoundaryWall(position, direction)) {
+      return false;
+    }
+    
+    const element = this.getCell(position);
+    element[direction] = DOOR;
+
+    const neighbor = this.getNeighbor(position, direction);
+    neighbor[opposite(direction)] = DOOR;
+    return true;
+  }
+
+  getNeighbor(position, direction) {
+    const neighborPosition = getAdjacentPosition(position, direction);
+    return this.getCell(neighborPosition);
+  }
+
+  getCell(position) { return this.maze[position.row][position.col]; }
+
+  hasBoundaryWall(position, direction) {
+    const row = position.row;
+    const col = position.col;
+    
+    switch(direction) {
+      case Direction.LEFT: return col == 0;
+      case Direction.TOP: return row == 0;
+      case Direction.RIGHT: return col == this.size - 1;
+      case Direction.BOTTOM: return row == this.size - 1;
+      default: return true;
+    }
+  }
+}
---
+++ index.js
@@ -0,0 +1,82 @@
+const canvas = document.getElementById("content");
+let ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = 'brown';
+
+const n = 30;
+const size = 500 / n;
+
+function toLeft(start) { return { x: start.x - size, y: start.y } }
+function toTop(start) { return { x: start.x, y: start.y - size } }
+
+function toRight(start) { return { x: start.x + size, y: start.y } }
+function toBottom(start) { return { x: start.x, y: start.y + size } }
+
+function drawLine(from, to) {
+  ctx.beginPath();
+  ctx.moveTo(from.x, from.y);
+  ctx.lineTo(to.x, to.y);
+  ctx.stroke();
+}
+  
+function labelToStyle(label) {
+  switch(label) {
+    case 0: return 'white';
+    case 1: return 'lawngreen';
+    case 2: return 'crimson';
+    default: return 'grey';
+  }
+}
+
+function drawCell(row, col, element) {
+  const topLeft = { x: col * size, y: row * size };
+  const bottomRight = { x: topLeft.x + size, y: topLeft.y + size };
+  if (element.left == WALL) drawLine(topLeft, toBottom(topLeft));
+  if (element.top == WALL) drawLine(topLeft, toRight(topLeft));
+  if (element.right == WALL) drawLine(bottomRight, toTop(bottomRight));
+  if (element.bottom == WALL) drawLine(bottomRight, toLeft(bottomRight));
+}
+
+function fillCell(row, col, element, maze) {
+  const center = size / 2;
+  const x = col * size;
+  const y = row * size;
+  let label = element.label;
+  if (maze.entrance.row == row && maze.entrance.col == col) {
+    label = 1;
+  } else if (maze.exit.row == row && maze.exit.col == col) {
+    label = 2;
+  }
+  ctx.fillStyle = labelToStyle(label);
+  ctx.fillRect(x + (center/2), y + (center/2), size / 2, size / 2);
+}
+
+// drawLine({x: 0, y: 0}, {x: size, y: 0});
+
+function cell(left, top, right, bottom) { return { left, top, right, bottom }}
+
+function emptyCell() { return new Cell(WALL, WALL, WALL, WALL); }
+
+// const maze = [
+//   [ new Cell(WALL, WALL, DOOR, WALL), new Cell(DOOR, WALL, WALL, DOOR), new Cell(WALL, WALL, WALL, WALL) ],
+//   [ new Cell(WALL, WALL, WALL, WALL), new Cell(WALL, DOOR, WALL, WALL), new Cell(WALL, WALL, WALL, WALL) ],
+//   [ new Cell(WALL, WALL, WALL, WALL), new Cell(WALL, WALL, WALL, WALL), new Cell(WALL, WALL, WALL, WALL) ]
+// ]
+
+const maze = new Maze(n);
+
+setInterval(() => {
+  maze.maze.forEach((row, i) => {
+    row.forEach((element, j) => {
+      drawCell(i, j, element);
+      fillCell(i, j, element, maze);
+    })
+  })
+}, 100);
+
+async function demo() {
+  await maze.makeSolvable();
+  console.log("Im back");
+}
+
+demo();
